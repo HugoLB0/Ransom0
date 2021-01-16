73b31e06f6c9de7ef91e899baebbd8bcad365f6b
Hugo LB <66400773+HugoLB0@users.noreply.github.com>
2021-01-16 10:52:24 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import pytest
+def test_file1_method1():
+	x=5
+	y=6
+	assert x+1 == y,"test failed"
+	assert x == y,"test failed"
+def test_file1_method2():
+	x=5
+	y=6
+	assert x+1 == y,"test failed" 
